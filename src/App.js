bcc2416481737f7918c3a1ddd97739a18bde410c
RiyadMia <96831477+RiyadMia@users.noreply.github.com>
2022-03-26 06:55:15 +0600

    how to react work ans addad

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
     <div className='App'>
       <h1>Mobile Shopping</h1>
       <Shop></Shop>
+      <h1>How to react work ?</h1>
+      <div>
+        <p>
+        React is a JavaScript library for building user interfaces.React is used to build single-page applications.React allows us to create reusable UI components.React creates a  virtual dom in memory.React creates a virtual dom in memory.The quickest way start learning React is to write React directly in your HTML files.React finds out what changes have been made, and changes only what needs to be changed.
+        </p>
+      </div>
+      <h1>How to useState work ? </h1>
+      <div>
+        <p>The useState is a Hook that allows you to have state variables in functional components.The useState hook is a special function that takes the initial state as an argument and returns an array of two entries. The above function is oneline function which computes the sum of two numbers and will be set as the initial state.The only argument to the useState() Hook is the initial state. Unlike with classes, the state doesn’t have to be an object. We can keep a number or a string if that’s all we need. </p>
+      </div>
     </div>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,24 +2,23 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const {cart}=props;
-    let total =0;
+    let name=''
     let quantity=0;
-    let name=[]
     for(const product of cart){
-        name = product.name
+        // console.log(cart[2].name)
+    //    n
+    name=product.name
         quantity=quantity+product.
         quantity;
-        total = total + product.price * product.quantity;
     }
     return (
         <div className='cart'>
             <h3>Mobile Order </h3>
             <p>Selected items : {quantity} </p>
             <h3> {name}</h3>
-            <p>Total price : $ {total} </p>
             <button className='btn-choice'>CHO0CE FOR 1 ME </button>
             <br />
-            <button className='btn-close'>CHOOCE AGAIN</button>
+            <button onClick={()=>props.removeCart()}   className='btn-close'>CHOOCE AGAIN</button>
         </div>
     );
 };
